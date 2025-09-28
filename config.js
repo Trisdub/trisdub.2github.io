32148c51eda1f9ba5327e724bc25ac8b5d0f1968
Trisdub <trisdubois@icloud.com>
2025-09-28 15:28:59 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase configuration (modular SDK v9+)
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfVProG4-iZ8z4r869oiBHB7OX4Fl3lMU",
+  authDomain: "volleyball-tourneys.firebaseapp.com",
+  projectId: "volleyball-tourneys",
+  storageBucket: "volleyball-tourneys.firebasestorage.app",
+  messagingSenderId: "652764056781",
+  appId: "1:652764056781:web:70f90b5a6f5357aff16d84",
+  measurementId: "G-HJF63771D7"
+};
